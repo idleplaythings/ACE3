970ff8ead20b7e5a1c3d2f0372fc2b45998967cc
Aatu <aatu.riuttamaki@gmail.com>
2015-06-19 22:06:36 +0300

    Ikarus changes
    Conflicts:
    	optionals/compat_hlc_ar15/CfgWeapons.hpp
    	optionals/compat_hlcmods_m14/CfgWeapons.hpp

--- optionals/compat_hlc_ar15/CfgWeapons.hpp
+++ optionals/compat_hlc_ar15/CfgWeapons.hpp
@@ -1,7 +1,16 @@
+class WeaponSlotsInfo;
+class Single;
+class Burst;
+class FullAuto;
+class Mode_SemiAuto;
+class Mode_Burst;
+class Mode_FullAuto;
+
 class CfgWeapons
 {
 	class Rifle;
 	class Rifle_Base_F;
+	
 	class hlc_ar15_base: Rifle_Base_F
 	{
         ACE_barrelTwist=177.8;
@@ -11,55 +20,129 @@ class CfgWeapons
 	{
 		ACE_barrelTwist=177.8;
 		ACE_barrelLength=261.62;
+        dexterity=1.6;
+		inertia=0.4;
+
+		class Single: Mode_SemiAuto { dispersion = 0.0013; reloadTime = 0.075; };
+        class FullAuto: Mode_FullAuto {	dispersion = 0.0013; reloadTime = 0.075; };
+		class WeaponSlotsInfo: WeaponSlotsInfo { mass=85; };
 	};
 	class hlc_rifle_RU5562: hlc_rifle_RU556
 	{
 		ACE_barrelTwist=177.8;
 		ACE_barrelLength=261.62;
+        dexterity=1.7;
+		inertia=0.35;
+
+		class Single: Mode_SemiAuto { dispersion = 0.0014; reloadTime = 0.075; };
+        class FullAuto: Mode_FullAuto {	dispersion = 0.0014; reloadTime = 0.075; };
+		class WeaponSlotsInfo: WeaponSlotsInfo { mass=85; };
 	};
 	class hlc_rifle_CQBR: hlc_rifle_RU556
 	{
 		ACE_barrelTwist=177.8;
 		ACE_barrelLength=254.0;
+        dexterity=1.7;
+		inertia=0.3;
+
+		class Single: Mode_SemiAuto { dispersion = 0.00185;  reloadTime = 0.075; };
+        class FullAuto: Mode_FullAuto {	dispersion = 0.00185;  reloadTime = 0.075; };
+		class WeaponSlotsInfo: WeaponSlotsInfo { mass=75; };
 	};
 	class hlc_rifle_M4: hlc_rifle_RU556
 	{
 		ACE_barrelTwist=177.8;
 		ACE_barrelLength=368.3;
+        dexterity=1.6;
+		inertia=0.4;
+
+		class Single: Mode_SemiAuto { dispersion = 0.0015; reloadTime = 0.075; };
+        class FullAuto: Mode_FullAuto {	dispersion = 0.0015; reloadTime = 0.075; };
+		class WeaponSlotsInfo: WeaponSlotsInfo { mass=80; };
+	};
+	class hlc_rifle_m4m203: hlc_rifle_M4
+	{
+        dexterity=1.5;
+		inertia=0.5;
+
+		class Single: Mode_SemiAuto { dispersion = 0.0015; reloadTime = 0.075; };
+        class FullAuto: Mode_FullAuto {	dispersion = 0.0015; reloadTime = 0.075; };
+		class WeaponSlotsInfo: WeaponSlotsInfo { mass=100; };
 	};
 	class hlc_rifle_bcmjack: hlc_ar15_base
 	{
 		ACE_barrelTwist=177.8;
 		ACE_barrelLength=368.3;
+        dexterity=1.5;
+		inertia=0.5;
+
+		class Single: Mode_SemiAuto { dispersion = 0.00116; reloadTime = 0.075; };
+        class FullAuto: Mode_FullAuto {	dispersion = 0.00116; reloadTime = 0.075; };
+		class WeaponSlotsInfo: WeaponSlotsInfo { mass=95; };
 	};
 	class hlc_rifle_Colt727: hlc_ar15_base
 	{
 		ACE_barrelTwist=177.8;
 		ACE_barrelLength=368.3;
+        dexterity=1.5;
+		inertia=0.5;
+
+		class Single: Mode_SemiAuto { dispersion = 0.00155; reloadTime = 0.086; };
+        class FullAuto: Mode_FullAuto {	dispersion = 0.00155; reloadTime = 0.086; };
+		class WeaponSlotsInfo: WeaponSlotsInfo { mass=85; };
 	};
 	class hlc_rifle_Colt727_GL: hlc_rifle_Colt727
 	{
 		ACE_barrelTwist=177.8;
 		ACE_barrelLength=368.3;
+        dexterity=1.4;
+		inertia=0.6;
+		
+		class WeaponSlotsInfo: WeaponSlotsInfo { mass=115; };
 	};
 	class hlc_rifle_Bushmaster300: hlc_rifle_Colt727
 	{
 		ACE_barrelTwist=203.2;
 		ACE_barrelLength=368.3;
+        dexterity=1.3;
+		inertia=0.5;
+
+		class Single: Mode_SemiAuto { dispersion = 0.0015; reloadTime = 0.075; };
+        class FullAuto: Mode_FullAuto {	dispersion = 0.0015; reloadTime = 0.075; };
+		class WeaponSlotsInfo: WeaponSlotsInfo { mass=100; };
 	};
 	class hlc_rifle_vendimus: hlc_rifle_Bushmaster300
 	{
 		ACE_barrelTwist=203.2;
 		ACE_barrelLength=406.4;
+        dexterity=1.25;
+		inertia=0.55;
+
+		class Single: Mode_SemiAuto { dispersion = 0.0014; reloadTime = 0.086; };
+        class FullAuto: Mode_FullAuto {	dispersion = 0.0014; reloadTime = 0.086; };
+		class WeaponSlotsInfo: WeaponSlotsInfo { mass=105; };
 	};
 	class hlc_rifle_SAMR: hlc_rifle_RU556
 	{
         ACE_barrelTwist=228.6;
         ACE_barrelLength=406.4;
+        dexterity=1.2;
+		inertia=0.55;
+
+		class Single: Mode_SemiAuto { dispersion = 0.0009; reloadTime = 0.086; };
+        class FullAuto: Mode_FullAuto {	dispersion = 0.0009; reloadTime = 0.086; };
+		class WeaponSlotsInfo: WeaponSlotsInfo { mass=105; };
 	};
+
 	class hlc_rifle_honeybase: hlc_rifle_RU556
 	{
         ACE_barrelTwist=203.2;
         ACE_barrelLength=152.4;
+        dexterity=1.7;
+		inertia=0.3;
+
+		class Single: Mode_SemiAuto { dispersion = 0.00185; reloadTime = 0.086 };
+        class FullAuto: Mode_FullAuto {	dispersion = 0.00185; reloadTime = 0.086 };
+		class WeaponSlotsInfo: WeaponSlotsInfo { mass=80; };
 	};
 };
--- optionals/compat_hlcmods_m14/CfgWeapons.hpp
+++ optionals/compat_hlcmods_m14/CfgWeapons.hpp
@@ -1,16 +1,41 @@
+class WeaponSlotsInfo;
+class Single;
+class Burst;
+class FullAuto;
+class Mode_SemiAuto;
+class Mode_Burst;
+class Mode_FullAuto;
 
 class CfgWeapons
 {
 	class Rifle_Base_F;
-    class hlc_rifle_M14;
 	class hlc_M14_base: Rifle_Base_F
 	{
 		ACE_barrelTwist=304.8;
 		ACE_barrelLength=558.8;
 	};
+
+	class hlc_rifle_M14: hlc_M14_base {
+		dexterity=1.6;
+		inertia=0.8;
+		magazines[]=
+		{
+			"20Rnd_762x51_Mag"
+		};
+
+		class Single: Mode_SemiAuto { dispersion = 0.00069; };
+        class FullAuto: Mode_FullAuto {	dispersion = 0.00069; };
+        class WeaponSlotsInfo: WeaponSlotsInfo { mass=130; };
+	};
+
 	class hlc_rifle_m14sopmod: hlc_rifle_M14
 	{
 		ACE_barrelTwist=304.8;
 		ACE_barrelLength=457.2;
+
+		magazines[]=
+		{
+			"20Rnd_762x51_Mag"
+		};
     };
 };
